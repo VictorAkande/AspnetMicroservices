cfc7ace34ded604591b488dd1e7b0b78a285cf78
Victor Oluwatobi Akande <akandevictor846@gmail.com>
2023-11-13 14:47:17 +0100

    ::

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductsById(string Id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task DeleteProduct(string productId);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext;
+        }
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _catalogContext
+                     .Products
+                     .Find(p=>true)
+                     .ToListAsync();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name, name);
+
+            return await _catalogContext
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
+;        }
+
+        public async Task<Product> GetProductsById(string Id)
+        {
+            return await _catalogContext
+                            .Products
+                            .Find(p => p.Id == Id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
